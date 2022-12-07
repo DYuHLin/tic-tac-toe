47f77c17f5a8bef37484f5b9bc60032f9e03deec
DYuHLin <damianver2000@gmail.com>
2022-12-07 15:30:57 +0200

    added functions for displaying on the board, fixed my gameboard module and added attributes to tiles on board

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-'use strict'
+const block = document.querySelectorAll(".block");
+let turnO = false;
 
 //the module for the gameboard
 const gameBoard = (() => {
     let board = ['', '', '', '', '', '', '', '', ''];
-    const block1 = document.getElementById("block-0");
-    const block2 = document.getElementById("block-1");
-    const block3 = document.getElementById("block-2");
-    const block4 = document.getElementById("block-3");
-    const block5 = document.getElementById("block-4");
-    const block6 = document.getElementById("block-5");
-    const block7 = document.getElementById("block-6");
-    const block8 = document.getElementById("block-7");
-    const block9 = document.getElementById("block-8");
+    
+    const winningCombo = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    return {board, winningCombo};
 })();
 
 //factory function for players
@@ -21,25 +26,34 @@ const playerCreator = (name, item) => {
 
 //factory function for game logic
 function game(){
+
+    const gameBoardObj = document.querySelector(".board");
+
+    const restartBtn = document.getElementById("restart-btn");
+
+    block.forEach(cell => cell.addEventListener('click', gameBoardSelect));
+    restartBtn.addEventListener('click', restartGame);
+   
+};
+
+function gameBoardSelect(){
+    const tiles = this.getAttribute("blockPos");
+    if(!gameBoard.board[tiles] !== ""){return};
+
+    updateTile(this, tiles);
+};
+
+function updateTile(block, index){
     let player1 = playerCreator("Damian", "X");
     let player2 = playerCreator("Bot", "O");
+    gameBoard.board[index] = player1.item;
 
-    let turnO = false;
+    block.textContent = player1.item;
+};
 
-    const gameBoardObj = document.querySelector(".block");
+function restartGame(){
 
-    gameBoardObj.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        if(turnO === false){
-            gameBoardObj.textContent = player1.item;
-            turnO = true;
-        } else if(turnO === true){
-            gameBoardObj.textContent = player2.item;
-            turnO = false;
-        };
-        
-    });
 };
 
 game();
+//gameBoardSelect()
